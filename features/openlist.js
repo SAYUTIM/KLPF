80180613c8ffa55ac74b30cc2ea26b970667cd50
SAYU <108171999+SAYUTIM@users.noreply.github.com>
2025-06-26 20:32:33 +0900

    Delete features/openlist.js

--- features/openlist.js
+++
@@ -1,5 +0,0 @@
-//Copyright (c) 2024 SAYU
-//This software is released under the MIT License, see LICENSE.
-
-const open = document.querySelector("span[id=\"allOpenTxt\"]");
-if (open && open.textContent.trim() !== "すべて閉じる") open.click();
